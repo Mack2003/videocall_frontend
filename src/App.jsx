a806cd486deaac97c111ebe169572e253f47e5b4
Mack2003 <moloyroy8906144923@gmail.com>
2024-06-28 15:53:23 +0530

    video elemnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { io } from 'socket.io-client';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [videoElement, setVideoElement] = useState(undefined);
+
+  const sockit = io("http://localhost:5200");
+  useEffect(()=>{
+    setVideoElement(document.getElementById('video'));
+  },[]);
+
+  const handelClick = () => {
+    sockit.emit('message', 'hello')
+    window.navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(chunks =>{
+      videoElement.src = chunks;
+    });
+  };
 
   return (
     <>
-      
+      <button
+      className='p-3 bg-green-600 rounded-lg m-5' 
+      onClick={handelClick}>send msg</button>
+      <video 
+      className='h-[10rem] w-[10rem] border-2 border-red-700'
+      id='video' 
+      autoPlay 
+      playsInline></video>
     </>
   )
 }
